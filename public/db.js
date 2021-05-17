97e3071f3ed7ce3b721f75a1f1120d675a1bad1e
AllisonWonderland <aneary@gmail.com>
2021-05-17 12:39:19 -0500

    finan changes

--- public/db.js
+++ public/db.js
@@ -1,11 +1,30 @@
 let db;
 
-//
+//this is to create a new db request from the budget db
 const request = indexedDB.open("budget", 1);
-
+// the onupgreadeneeded property of the IDBOpenDBRequest interface is the event 
+// handler for the upgradeneeded event. This is triggered when the database of the larger
+// version number than the existing stored database is loaded. 
 request.onupgreadeneeded = ({ event }) => {
   const db = event.result;
 
+//   this is to create an object called 'pending'
   db.createObjectStore("pending", { auto_increment: true });
 };
+
+request.onsuccess = ({event}) => {
+    db = event.result;
+    if (navigator.onLine){
+        checkDatabase();
+    }
+}
+request.onerror = function(error) {
+    console.log("Show me the error,if there is one");
+};
+function checkDatabase() {
+    // this is to start a new transaction with pending dbTransaction
+    const transaction = db.transaction(['pending'], 'readwrite');
+
+    const store = transaction.objectstore
+}
 
